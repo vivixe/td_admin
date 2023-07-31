5ca675c7e86f91ee04ebd3f3005f22e61718836a
vivixe <1104760234@qq.com>
2023-07-31 23:56:46 +0800

    feat(compiler): add 'comments' option

--- src/pages/WorkerList/index.tsx
+++ src/pages/WorkerList/index.tsx
@@ -1,234 +1,233 @@
+import { getWorkerList } from '@/services/admin/worker';
 import { PlusOutlined } from '@ant-design/icons';
 import {
-    // FooterToolbar,
-    ModalForm,
-    PageContainer,
-    // ProDescriptions,
-    ProFormText,
-    ProFormTextArea,
-    ProTable,
+  // FooterToolbar,
+  ModalForm,
+  PageContainer,
+  // ProDescriptions,
+  ProFormText,
+  ProFormTextArea,
+  ProTable,
 } from '@ant-design/pro-components';
-import { Button } from 'antd';
-import { getWorkerList } from '@/services/admin/worker'
 import type { ActionType, ProColumns } from '@ant-design/pro-table';
 import { FormattedMessage, useIntl } from '@umijs/max';
+import { Button } from 'antd';
 import React, { useRef, useState } from 'react';
 import WorkerForm from './components/WorkerForm';
 
 const handleAdd = (fields: API.WorkerListItem) => {
-    console.log(fields);
-    return true;
-}
+  console.log(fields);
+  return true;
+};
 
 const handleUpdate = (fields: API.WorkerListItem) => {
-    console.log(fields);
-    return true;
-}
+  console.log(fields);
+  return true;
+};
 
 const WorkerList: React.FC = () => {
+  const [createModalOpen, handleModalOpen] = useState<boolean>(false);
 
-    const [createModalOpen, handleModalOpen] = useState<boolean>(false);
+  const [updateModalOpen, handleUpdateModalOpen] = useState<boolean>(false);
 
-    const [updateModalOpen, handleUpdateModalOpen] = useState<boolean>(false);
+  const actionRef = useRef<ActionType>();
+  const [currentRow, setCurrentRow] = useState<API.WorkerListItem>();
 
-    const actionRef = useRef<ActionType>();
-    const [currentRow, setCurrentRow] = useState<API.WorkerListItem>();
+  const intl = useIntl();
 
-    const intl = useIntl();
-
-    const columns: ProColumns<API.WorkerListItem>[] = [
-        {
-            title: (
-                <FormattedMessage
-                    id="pages.workerTable.workerName"
-                    defaultMessage="职员姓名"
-                />
-            ),
-            dataIndex: 'name',
-            tip: '规则名称是唯一的 key',
-            render: (dom, entity) => {
-                return (
-                    <a
-                        onClick={() => {
-                            setCurrentRow(entity);
-                        }}
-                    >
-                        {dom}
-                    </a>
-                );
-            }
+  const columns: ProColumns<API.WorkerListItem>[] = [
+    {
+      title: <FormattedMessage id="pages.workerTable.workerName" defaultMessage="职员姓名" />,
+      dataIndex: 'name',
+      tip: '规则名称是唯一的 key',
+      render: (dom, entity) => {
+        return (
+          <a
+            onClick={() => {
+              setCurrentRow(entity);
+            }}
+          >
+            {dom}
+          </a>
+        );
+      },
+    },
+    {
+      title: <FormattedMessage id="pages.workerTable.workerPosition" defaultMessage="职位" />,
+      dataIndex: 'position_name',
+      render: (dom, entity) => {
+        return (
+          <div
+            onClick={() => {
+              setCurrentRow(entity);
+            }}
+          >
+            {dom}
+          </div>
+        );
+      },
+    },
+    {
+      title: <FormattedMessage id="pages.workerTable.workerContact" defaultMessage="联系方式" />,
+      dataIndex: 'contact',
+      render: (dom, entity) => {
+        return (
+          <div
+            onClick={() => {
+              setCurrentRow(entity);
+            }}
+          >
+            {/* {dom} */}
+            {entity.phone}
+            {entity.email}
+          </div>
+        );
+      },
+    },
+    {
+      title: <FormattedMessage id="pages.searchTable.titleStatus" defaultMessage="状态" />,
+      dataIndex: 'status',
+      hideInForm: true,
+      valueEnum: {
+        default: {
+          text: (
+            <FormattedMessage
+              id="pages.searchTable.nameStatus.default"
+              defaultMessage="Shut down"
+            />
+          ),
+          status: 'Default',
         },
-        {
-            title: (
-                <FormattedMessage
-                    id="pages.workerTable.workerPosition"
-                    defaultMessage="职位"
-                />
-            ),
-            dataIndex: 'position',
-            render: (dom, entity) => {
-                return (
-                    <div
-                        onClick={() => {
-                            setCurrentRow(entity);
-                        }}
-                    >
-                        {dom}
-                    </div>
-                );
-            }
+        1: {
+          text: (
+            <FormattedMessage id="pages.searchTable.nameStatus.running" defaultMessage="Running" />
+          ),
+          status: 'Processing',
         },
-        {
-            title: (
-                <FormattedMessage
-                    id="pages.workerTable.workerContact"
-                    defaultMessage="联系方式"
-                />
-            ),
-            dataIndex: 'contact',
-            render: (dom, entity) => {
-                return (
-                    <div
-                        onClick={() => {
-                            setCurrentRow(entity);
-                        }}
-                    >
-                        {dom}
-                    </div>
-                );
-            }
+        2: {
+          text: (
+            <FormattedMessage id="pages.searchTable.nameStatus.online" defaultMessage="Online" />
+          ),
+          status: 'Success',
         },
-        {
-            title: (
+        3: {
+          text: (
+            <FormattedMessage
+              id="pages.searchTable.nameStatus.abnormal"
+              defaultMessage="Abnormal"
+            />
+          ),
+          status: 'Error',
+        },
+      },
+    },
+    {
+      title: <FormattedMessage id="pages.searchTable.titleOption" defaultMessage="操作" />,
+      dataIndex: 'option',
+      valueType: 'option',
+      render: (_, record) => [
+        <a
+          key="config"
+          onClick={() => {
+            handleUpdateModalOpen(true);
+            setCurrentRow(record);
+          }}
+        >
+          <FormattedMessage id="pages.searchTable.config" defaultMessage="配置" />
+        </a>,
+        <a key="subscribeAlert" href="https://procomponents.ant.design/">
+          <FormattedMessage id="pages.searchTable.subscribeAlert" defaultMessage="订阅警报" />
+        </a>,
+      ],
+    },
+  ];
+
+  return (
+    <PageContainer>
+      <ProTable<API.WorkerListItem, API.PageParams>
+        headerTitle={intl.formatMessage({
+          id: 'pages.searchTable.title',
+          defaultMessage: '查询表格',
+        })}
+        actionRef={actionRef}
+        rowKey="key"
+        search={{
+          labelWidth: 120,
+        }}
+        toolBarRender={() => [
+          <Button
+            type="primary"
+            key="primary"
+            onClick={() => {
+              handleModalOpen(true);
+            }}
+          >
+            <PlusOutlined /> <FormattedMessage id="pages.searchTable.new" defaultMessage="New" />
+          </Button>,
+        ]}
+        request={getWorkerList}
+        columns={columns}
+        rowSelection={{}}
+      />
+      <ModalForm
+        title={intl.formatMessage({
+          id: 'pages.searchTable.createForm.newRule',
+          defaultMessage: 'New rule',
+        })}
+        width="400px"
+        open={createModalOpen}
+        onOpenChange={handleModalOpen}
+        onFinish={async (value) => {
+          const success = await handleAdd(value as API.WorkerListItem);
+          if (success) {
+            handleModalOpen(false);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+        }}
+      >
+        <ProFormText
+          rules={[
+            {
+              required: true,
+              message: (
                 <FormattedMessage
-                    id="pages.searchTable.titleStatus"
-                    defaultMessage="状态"
+                  id="pages.searchTable.ruleName"
+                  defaultMessage="Rule name is required"
                 />
-            ),
-            dataIndex: 'status',
-            hideInForm: true,
-            valueEnum: {
-                0: {
-                    text: (
-                        <FormattedMessage
-                            id="pages.searchTable.nameStatus.default"
-                            defaultMessage="Shut down"
-                        />
-                    ),
-                    status: 'Default',
-                },
-                1: {
-                    text: (
-                        <FormattedMessage id="pages.searchTable.nameStatus.running" defaultMessage="Running" />
-                    ),
-                    status: 'Processing',
-                },
-                2: {
-                    text: (
-                        <FormattedMessage id="pages.searchTable.nameStatus.online" defaultMessage="Online" />
-                    ),
-                    status: 'Success',
-                },
-                3: {
-                    text: (
-                        <FormattedMessage
-                            id="pages.searchTable.nameStatus.abnormal"
-                            defaultMessage="Abnormal"
-                        />
-                    ),
-                    status: 'Error',
-                },
+              ),
             },
-        }
+          ]}
+          width="md"
+          name="name"
+        />
+        <ProFormTextArea width="md" name="desc" />
+      </ModalForm>
+      <WorkerForm
+        onSubmit={async (value) => {
+          const success = await handleUpdate(value);
+          if (success) {
+            handleUpdateModalOpen(false);
+            setCurrentRow(undefined);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+          console.log(value);
+        }}
+        onCancel={() => {
+          handleUpdateModalOpen(false);
+          // if (!showDetail) {
+          //     setCurrentRow(undefined);
+          // }
+        }}
+        updateModalOpen={updateModalOpen}
+        values={currentRow || {}}
+        name={currentRow?.name || ''}
+      />
+    </PageContainer>
+  );
+};
 
-    ]
-
-    return (
-        <PageContainer>
-            <ProTable<API.WorkerListItem, API.PageParams>
-                headerTitle={intl.formatMessage({
-                    id: 'pages.searchTable.title',
-                    defaultMessage: '查询表格',
-                })}
-                actionRef={actionRef}
-                rowKey="key"
-                search={{
-                    labelWidth: 120,
-                }}
-                toolBarRender={() => [
-                    <Button
-                        type="primary"
-                        key="primary"
-                        onClick={() => {
-                            handleModalOpen(true);
-                        }}
-                    >
-                        <PlusOutlined /> <FormattedMessage id="pages.searchTable.new" defaultMessage="New" />
-                    </Button>,
-                ]}
-                request={getWorkerList}
-                columns={columns}
-                rowSelection={{}}
-            />
-            <ModalForm
-                title={intl.formatMessage({
-                    id: 'pages.searchTable.createForm.newRule',
-                    defaultMessage: 'New rule',
-                })}
-                width="400px"
-                open={createModalOpen}
-                onOpenChange={handleModalOpen}
-                onFinish={async (value) => {
-                    const success = await handleAdd(value as API.WorkerListItem);
-                    if (success) {
-                        handleModalOpen(false);
-                        if (actionRef.current) {
-                            actionRef.current.reload();
-                        }
-                    }
-                }}
-            >
-                <ProFormText
-                    rules={[
-                        {
-                            required: true,
-                            message: (
-                                <FormattedMessage
-                                    id="pages.searchTable.ruleName"
-                                    defaultMessage="Rule name is required"
-                                />
-                            ),
-                        },
-                    ]}
-                    width="md"
-                    name="name"
-                />
-                <ProFormTextArea width="md" name="desc" />
-            </ModalForm>
-            <WorkerForm
-                onSubmit={async (value) => {
-                    const success = await handleUpdate(value);
-                    if (success) {
-                        handleUpdateModalOpen(false);
-                        setCurrentRow(undefined);
-                        if (actionRef.current) {
-                            actionRef.current.reload();
-                        }
-                    }
-                    console.log(value);
-                }}
-                onCancel={() => {
-                    handleUpdateModalOpen(false);
-                    // if (!showDetail) {
-                    //     setCurrentRow(undefined);
-                    // }
-                }}
-                updateModalOpen={updateModalOpen}
-                values={currentRow || {}}
-                name={currentRow?.name || ''}
-            />
-        </PageContainer>
-    )
-}
-
-export default WorkerList
+export default WorkerList;
