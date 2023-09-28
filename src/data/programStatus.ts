436fbeac3750295159359a81fb8f25695da96ffe
vivixue <1104760234@qq.com>
2023-09-28 17:00:41 +0800

    提交

---
+++ src/data/programStatus.ts
@@ -0,0 +1,37 @@
+export const programStatus = [
+	{
+		label: '未开始',
+		value: 0,
+        color: '#76b6e4',
+	},
+	{
+		label: '规划中',
+		value: 1,
+        color: '#6b90de',
+	},
+	{
+		label: '开发中',
+		value: 5,
+        color: '#abd46e',
+	},
+	{
+		label: '测试中',
+		value: 10,
+        color: '#6543a4',
+	},
+    {
+        label: '暂停中',
+        value: 20,
+        color: '#de8438',
+    },
+	{
+		label: '已上线',
+		value: 50,
+        color: '#639a72',
+	},
+	{
+		label: '已结束',
+		value: 100,
+        color: '#61565a',
+	},
+]
--- src/pages/ProgramAdd/index.tsx
+++ src/pages/ProgramAdd/index.tsx
@@ -2,24 +2,30 @@ import React, { useState, useEffect } from 'react';
 import ProForm, {
     ProFormText,
     ProFormDatePicker,
-    ProFormSelect
+    ProFormSelect,
+    ProFormDigit
 } from '@ant-design/pro-form';
 import { PageContainer } from '@ant-design/pro-components'
 import {
     Skeleton,
     Avatar,
     Button,
+    Tag,
+    Upload,
+    Modal
 } from 'antd';
-import {UsergroupAddOutlined} from '@ant-design/icons'
+import { UsergroupAddOutlined } from '@ant-design/icons'
 import { isEmpty } from 'lodash';
-import { getProgramInfo,getUserSelect } from '@/services/admin/program'
+import { getProgramInfo, getUserSelect } from '@/services/admin/program'
 import { getTeamList } from '@/services/admin/team';
 import { useLocation } from '@umijs/max'
+import { programStatus } from '@/data/programStatus';
+import type { UploadFile } from 'antd/es/upload/interface';
 
 const ProgramAdd = () => {
     // 只有通过校验之后才会触发这个方法
     const onSubmit = async (values: object) => {
-        console.log(values);
+        console.log('%c [ values ]-25', 'font-size:16px; background:#133f21; color:#578365;', values)
     };
     // 详情数据
     const [detail, setDetail] = useState({});
@@ -30,116 +36,234 @@ const ProgramAdd = () => {
 
     // 获取详情
     useEffect(() => {
-        if(program_id === undefined) return
-        getProgramInfo({id:program_id}).then(res => {
-            if(res.status === 0){    
-                setDetail(res.data || {});
+        if (program_id === undefined) {
+            let initalData = {
+                status: "0"
             }
-        })
+            setDetail(initalData)
+        } else {
+            getProgramInfo({ id: program_id }).then(res => {
+                if (res.status === 0) {
+                    setDetail(res.data || {});
+                }
+            })
+        }
     }, []);
 
-    const loadingPage = isEmpty(detail) && program_id !== undefined;
+    interface OSSDataType {
+        dir: string;
+        expire: string;
+        host: string;
+        accessId: string;
+        policy: string;
+        signature: string;
+    }
+
+    interface AliyunOSSUploadProps {
+        value?: UploadFile[];
+        onChange?: (fileList: UploadFile[]) => void;
+    }
+
+    const AliyunOSSUpload = ({ value, onChange }: AliyunOSSUploadProps) => {
+        const [OSSData, setOSSData] = useState<OSSDataType>();
+      
+        // Mock get OSS api
+        // https://help.aliyun.com/document_detail/31988.html
+        const mockGetOSSData = () => ({
+          dir: 'user-dir/',
+          expire: '1577811661',
+          host: '//www.mocky.io/v2/5cc8019d300000980a055e76',
+          accessId: 'c2hhb2RhaG9uZw==',
+          policy: 'eGl4aWhhaGFrdWt1ZGFkYQ==',
+          signature: 'ZGFob25nc2hhbw==',
+        });
+      
+        const init = async () => {
+          try {
+            const result = await mockGetOSSData();
+            setOSSData(result);
+          } catch (error) {
+            message.error(error);
+          }
+        };
+      
+        useEffect(() => {
+          init();
+        }, []);
+      
+        const handleChange: UploadProps['onChange'] = ({ fileList }) => {
+          console.log('Aliyun OSS:', fileList);
+          onChange?.([...fileList]);
+        };
+      
+        const onRemove = (file: UploadFile) => {
+          const files = (value || []).filter((v) => v.url !== file.url);
+      
+          if (onChange) {
+            onChange(files);
+          }
+        };
+      
+        const getExtraData: UploadProps['data'] = (file) => ({
+          key: file.url,
+          OSSAccessKeyId: OSSData?.accessId,
+          policy: OSSData?.policy,
+          Signature: OSSData?.signature,
+        });
+      
+        const beforeUpload: UploadProps['beforeUpload'] = async (file) => {
+          if (!OSSData) return false;
+      
+          const expire = Number(OSSData.expire) * 1000;
+      
+          if (expire < Date.now()) {
+            await init();
+          }
+      
+          const suffix = file.name.slice(file.name.lastIndexOf('.'));
+          const filename = Date.now() + suffix;
+          // @ts-ignore
+          file.url = OSSData.dir + filename;
+      
+          return file;
+        };
+      
+        const uploadProps: UploadProps = {
+          name: 'file',
+          fileList: value,
+          action: OSSData?.host,
+          onChange: handleChange,
+          onRemove,
+          data: getExtraData,
+          beforeUpload,
+        };
+      
+        return (
+          <Upload {...uploadProps}>
+            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+          </Upload>
+        );
+      };
+      
+      const App: React.FC = () => (
+        <Form labelCol={{ span: 4 }}>
+          <Form.Item label="Photos" name="photos">
+            <AliyunOSSUpload />
+          </Form.Item>
+        </Form>
+      );
+
+    const loadingPage = isEmpty(detail);
 
     // 数据获取到之前展示加载动画，让 form 渲染时肯定可以得到初始值
     return <PageContainer
     >
-    {loadingPage ? (
-        <Skeleton active />
-    ) : (
-        <ProForm style={{backgroundColor:"#fff",padding: "20px"}} initialValues={detail} onFinish={onSubmit}>
-            {/* ... */}
-            <ProFormText
-                name="name"
-                label="名称"
-                width="md"
-                placeholder="请输入名称"
-                rules={[{ required: true, message: '名称不能为空' }]}
-            />
-            <ProFormSelect
-                name="owner"
-                label="负责人"
-                width="md"
-                fieldProps={{
-                    onChange: async (value) => {
-                        console.log('%c [ value ]-93', 'font-size:16px; background:#93b3bf; color:#f1d8ff;', value)
-                    },
-                }}
-                request={async () => {
-                    const data = await getUserSelect({ current: 1, pageSize: 1000 },);
-                    console.log('%c [ data ]-98', 'font-size:16px; background:#93b3bf; color:#f1d8ff;', data)
-                    // 取出data.data中的id和name
-                    const UserList = data.data ? data.data.map((item) => {
-                        // return { label: item.nickname, value: item.id }
+        {loadingPage ? (
+            <Skeleton active />
+        ) : (
+            <ProForm style={{ backgroundColor: "#fff", padding: "20px" }} initialValues={detail} onFinish={onSubmit}>
+                <ProFormText
+                    name="name"
+                    label="名称"
+                    width="md"
+                    placeholder="请输入名称"
+                    rules={[{ required: true, message: '名称不能为空' }]}
+                />
+                <ProFormSelect
+                    name="owner"
+                    label="负责人"
+                    width="md"
+                    fieldProps={{
+                        onChange: async (value) => {
+                            console.log('%c [ value ]-93', 'font-size:16px; background:#93b3bf; color:#f1d8ff;', value)
+                        },
+                    }}
+                    request={async () => {
+                        const data = await getUserSelect({ current: 1, pageSize: 1000 },);
+                        console.log('%c [ data ]-98', 'font-size:16px; background:#93b3bf; color:#f1d8ff;', data)
+                        // 取出data.data中的id和name
+                        const UserList = data.data ? data.data.map((item) => {
+                            // return { label: item.nickname, value: item.id }
+                            return {
+                                label: (
+                                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                                        <Avatar src={item.user_pic} size={24} style={{ marginRight: '8px' }}>{item.nickname}</Avatar>
+                                        {item.nickname}
+                                    </div>
+                                ),
+                                value: item.id + ""
+                            }
+                        }
+                        ) : [];
+                        return UserList;
+                    }}
+                    rules={[
+                        {
+                            required: true,
+                            message: (
+                                "负责人为必选项"
+                            ),
+                        },
+                    ]}
+                />
+                <ProFormSelect
+                    name="team_id"
+                    label="所属团队"
+                    width="md"
+                    fieldProps={{
+                        onChange: async (value) => {
+                            console.log('%c [ value ]-93', 'font-size:16px; background:#93b3bf; color:#f1d8ff;', value)
+                        },
+                    }}
+                    request={async () => {
+                        const data = await getTeamList()
+                        console.log('%c [ data ]-113', 'font-size:16px; background:#582a0b; color:#9c6e4f;', data)
+                        const TeamList = data.data ? data.data.map((item) => {
+                            // return { label: item.nickname, value: item.id }
+                            return {
+                                label: (
+                                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                                        <Avatar src={item.team_pic} size={24} style={{ marginRight: '8px' }}>{item.name}</Avatar>
+                                        {item.name}
+                                    </div>
+                                ),
+                                value: item.id
+                            }
+                        }
+                        ) : [];
+                        return TeamList;
+                    }}
+                    rules={[
+                        {
+                            required: true,
+                            message: (
+                                "所属团队为必选项"
+                            ),
+                        },
+                    ]}
+                />
+
+                <Button type="dashed" icon={<UsergroupAddOutlined />}>
+                    新建团队
+                </Button>
+
+                <ProFormSelect
+                    name="status"
+                    label="状态"
+                    width="md"
+                    options={programStatus.map(item => {
                         return {
                             label: (
-                                <div style={{display: 'flex',alignItems:'center'}}>
-                                    <Avatar src={item.user_pic} size={24} style={{marginRight:'8px'}}>{item.nickname}</Avatar>
-                                    {item.nickname}
+                                <div>
+                                    <Tag color={item.color}>{item.label}</Tag>
+                                    <span>{item.label}</span>
                                 </div>
-                            ), 
-                            value: item.id+"" }
-                    }
-                    ): [];
-                    return UserList;
-                }}
-                rules={[
-                    {
-                        required: true,
-                        message: (
-                            "负责人为必选项"
-                        ),
-                    },
-                ]}
-            />
-            <ProFormSelect
-                name="team_id"
-                label="所属团队"
-                width="md"
-                fieldProps={{
-                    onChange: async (value) => {
-                        console.log('%c [ value ]-93', 'font-size:16px; background:#93b3bf; color:#f1d8ff;', value)
-                    },
-                }}
-                request={async () => {
-                    const data = await getTeamList()
-                    console.log('%c [ data ]-113', 'font-size:16px; background:#582a0b; color:#9c6e4f;', data)
-                    const TeamList = data.data ? data.data.map((item) => {
-                        // return { label: item.nickname, value: item.id }
-                        return {
-                            label: (
-                                <div style={{display: 'flex',alignItems:'center'}}>
-                                    <Avatar src={item.team_pic} size={24} style={{marginRight:'8px'}}>{item.name}</Avatar>
-                                    {item.name}
-                                </div>
-                            ), 
-                            value: item.id }
-                    }
-                    ): [];
-                    return TeamList;
-                }}
-                rules={[
-                    {
-                        required: true,
-                        message: (
-                            "所属团队为必选项"
-                        ),
-                    },
-                ]}
-            >
-
-            </ProFormSelect>
-            <Button type="dashed" icon={<UsergroupAddOutlined />}>
-                新建团队
-            </Button>
-
-            <ProFormSelect
-                name="status"
-                label="状态"
-                width="md"
-            >
-
-            </ProFormSelect>
-            {/* <ProFormRadio.Group
+                            ),
+                            value: item.value + ""
+                        }
+                    })}
+                />
+                {/* <ProFormRadio.Group
                 name="status"
                 label="状态"
                 options={[
@@ -148,14 +272,32 @@ const ProgramAdd = () => {
                 ]}
                 rules={[{ required: true, message: '状态不能为空' }]}
             /> */}
-            <ProFormDatePicker
-                name="publishTime"
-                label="时间"
-                placeholder="请选择时间"
-                rules={[{ required: true, message: '时间不能为空' }]}
-            />
-        </ProForm>
-    )}
+
+                <ProFormDigit label="预估" name="earn" min={0} />
+
+                <ProFormDatePicker
+                    name="time"
+                    label="时间"
+                    placeholder="请选择时间"
+                    rules={[{ required: true, message: '时间不能为空' }]}
+                />
+
+                <Upload
+                    action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                    listType="picture-card"
+                // fileList={fileList}
+                // onPreview={handlePreview}
+                // onChange={handleChange}
+                >
+                    {/* {fileList.length >= 1 ? null : uploadButton} */}
+                </Upload>
+                <Modal
+                // open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                // <img alt="example" style={{ width: '100%' }} src={previewImage} /
+                >
+                </Modal>
+            </ProForm>
+        )}
     </PageContainer>
 };
 
